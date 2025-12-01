7da7956a8878e2d364b27d613737ec924394e8a2
2skydev <2skydev@gmail.com>
2025-12-01 13:17:59 +0900

    refactor: index 페이지 수정

--- src/renderer/src/pages/index.tsx
+++ src/renderer/src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { useTheme } from '@renderer/components/common/ThemeProvider'
 import Versions from '@renderer/components/common/Versions'
+import { Button } from '@renderer/components/ui/button'
 
 const HomePage = () => {
+  const { theme, setTheme } = useTheme()
+
   return (
     <div>
       <h1 className="text-2xl font-bold">Hello World</h1>
+
+      <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        Change to {theme === 'dark' ? 'Light' : 'Dark'} Theme
+      </Button>
+
       <Versions />
     </div>
   )
