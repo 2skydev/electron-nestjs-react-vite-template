c0147686b974c62dedaf51b70ba5e3426ef1d4e5
2skydev <2skydev@gmail.com>
2025-12-01 21:04:00 +0900

    fix: toaster 추가

--- src/renderer/src/pages/_app.tsx
+++ src/renderer/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
 import { ThemeProvider } from '@renderer/components/common/ThemeProvider'
+import { Toaster } from '@renderer/components/ui/sonner'
 
 const App = () => {
   return (
     <ThemeProvider>
+      <Toaster />
       <Outlet />
     </ThemeProvider>
   )
